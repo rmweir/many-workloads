93006507e3ed505c4d499161203f070074a21eb9
rmweir <ricardo.weir1@gmail.com>
2019-03-15 10:49:28 -0700

    Fix sum calculation

--- load_vars.sh
+++ load_vars.sh
@@ -5,7 +5,7 @@ START=$2
 
 for ((i=1;i<=$ITERATIONS;i++)); do
     echo \\\"Workload number: $i\\\"
-    let sum=($i + $START - 1)
+    let sum=($i + $START)
     let prior=($sum - 1)
     echo $prior
     python replace.py \"abcdefghr$prior\" \"abcdefghr$i\"
